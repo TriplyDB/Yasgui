e8309b961ad7a9d325e10c063c99e42c7f6cfec3
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-01-29 23:05:32 +0100

    Updated dist/docs

--- dist/yasgui.bundled.js
+++ dist/yasgui.bundled.js
@@ -30035,7 +30035,7 @@ module.exports = {
 				/**
 				 * Query string
 				 */
-				value : "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nSELECT * WHERE {\n  ?sub ?pred ?obj .\n} \nLIMIT 10",
+				value : "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nSELECT * WHERE {\n  ?sub ?pred ?obj .\n} \nLIMIT 10",
 				highlightSelectionMatches : {
 					showToken : /\w/
 				},
