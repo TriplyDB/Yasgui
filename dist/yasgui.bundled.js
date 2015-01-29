940e1f36820f476b29ff8c9d5e354a392ca2da30
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-01-29 15:34:54 +0100

    Updated dist/docs

--- dist/yasgui.bundled.js
+++ dist/yasgui.bundled.js
@@ -64780,11 +64780,10 @@ var defaultPersistent = {
 
 module.exports = function(yasgui, id, name) {
 	if (!yasgui.persistentOptions.tabManager.tabs[id]) {
-		yasgui.persistentOptions.tabManager.tabs[id] = {
+		yasgui.persistentOptions.tabManager.tabs[id] = $.extend(true, {
 			id: id,
-			name: name,
-			yasqe: defaultPersistentYasqe
-		}
+			name: name
+		}, defaultPersistent);
 	} else {
 		yasgui.persistentOptions.tabManager.tabs[id] = $.extend(true, {}, defaultPersistent, yasgui.persistentOptions.tabManager.tabs[id]);
 	}
