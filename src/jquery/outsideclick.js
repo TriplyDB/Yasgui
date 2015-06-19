158c6691ab9eb08bcf86b45d4907618cad820365
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-06-19 21:20:02 +0200

    fixed outside click to remove handler before a next handler might be attached. Fixes issue of tab context menu not opening

--- src/jquery/outsideclick.js
+++ src/jquery/outsideclick.js
@@ -7,24 +7,23 @@ $.fn.onOutsideClick = function(onOutsideClick, config) {
 		allowedElements: $()
 	}, config)
 	var el = $(this);
-	
+
 	var handler = function(e) {
 		var clickOutsideIssued = function(elCheck) {
 			return !elCheck.is(e.target) // if the target of the click isn't the container...
 			&& elCheck.has(e.target).length === 0  // ... nor a descendant of the container
 		};
-		
+
 		if (clickOutsideIssued(el) && clickOutsideIssued(config.allowedElements)) {
 			if (config.skipFirst) {
 				config.skipFirst = false;
 			} else {
 				onOutsideClick();
-				$(document).off('mouseup', handler);
+				$(document).off('mousedown', handler);
 			}
 		}
 	};
-	$(document).mouseup(handler);
-	
+	$(document).mousedown(handler);
+
 	return this;
 }
-
