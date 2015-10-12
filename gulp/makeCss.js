d07ad531c920a7f7b3c235dea953bd83a4c872d8
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-10-12 21:07:48 +0200

    auto-prefix css

--- gulp/makeCss.js
+++ gulp/makeCss.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 	paths = require('./paths.js'),
 	connect = require('gulp-connect'),
 	sourcemaps = require('gulp-sourcemaps');
+	autoprefixer = require('gulp-autoprefixer'),
 	sass = require('gulp-sass'),
 	cssImport = require('gulp-cssimport'),
 	rename = require("gulp-rename"),
@@ -17,14 +18,17 @@ gulp.task('makeCss', function() {
 	    .on("error", notify.onError(function(error) {
 	    	return error.message;
 	    }))
+			.pipe(autoprefixer({
+					browsers: ['> 5%']
+			}))
 	    .pipe(concat(paths.bundleName + '.css'))
 	    .pipe(gulp.dest(paths.bundleDir))
 	    .pipe(minifyCSS({
 			//the minifyer does not work well with lines including a comment. e.g.
-			///* some comment */ } 
+			///* some comment */ }
 			//is completely removed (including the final bracket)
 			//So, disable the 'advantaced' feature. This only makes the minified file 100 bytes larger
-			noAdvanced: true, 
+			noAdvanced: true,
 		}))
 	    .pipe(rename(paths.bundleName + '.min.css'))
 	    .pipe(gulp.dest(paths.bundleDir))
