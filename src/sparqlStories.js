4543f9ec38ee3eb2b535b8c53a48517b1733ebb3
Laurens Rietveld <laurens.rietveld@gmail.com>
2017-03-27 16:38:11 +0200

    updated sparqlstories so relative endpoint urls work as well

--- src/sparqlStories.js
+++ src/sparqlStories.js
@@ -1,7 +1,7 @@
 var $ = require("jquery");
 var linkUtils = require('./shareLink');
 var Promise = require('promise-polyfill');
-
+var urlParse = require('url-parse');
 module.exports = function() {
   $( document ).ready(function() {
     $('div[data-yasgui]').each(function(i) {
@@ -9,6 +9,9 @@ module.exports = function() {
       const url = $this.attr('data-yasgui');
       getFullUrl(url)
         .then(linkUtils.getOptionsFromUrl)
+        .then(function(options) {
+          return cleanConfig(options,url)
+        })
         .then(function(config) {
           initializeWrapper($this)
           window.$el = $this;
@@ -26,11 +29,24 @@ module.exports = function() {
           }
           window.yasgui = $this.yasgui;
         })
-        .then(console.log)
         .catch(console.error)
     })
 });
 
+}
+
+function cleanConfig(config, originalUrl) {
+  if (config.yasqe.sparql && config.yasqe.sparql.endpoint && config.yasqe.sparql.endpoint.indexOf('http') !== 0) {
+    //hmm, a relative path, do some magic to rewrite the endpoint
+    var parsedOriginalUrl = urlParse(originalUrl);
+    var parsedEndpointUrl = urlParse(config.yasqe.sparql.endpoint);
+    parsedOriginalUrl.set('hash', parsedEndpointUrl.hash)
+    parsedOriginalUrl.set('query', parsedEndpointUrl.query)
+    parsedOriginalUrl.set('pathname', parsedEndpointUrl.pathname)
+    config.yasqe.sparql.endpoint = parsedOriginalUrl.href;
+  }
+
+  return config;
 }
 
 function initializeWrapper($el, yasgui) {
