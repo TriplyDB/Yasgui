7bc07aa67597928c0dddf793647af0135527d4b8
Laurens Rietveld <laurens.rietveld@gmail.com>
2017-04-18 08:49:39 +0200

    load sparqlstories divs serially instead of parallel

--- src/sparqlStories.js
+++ src/sparqlStories.js
@@ -4,37 +4,52 @@ var Promise = require('promise-polyfill');
 var urlParse = require('url-parse');
 module.exports = function() {
   $( document ).ready(function() {
-    $('div[data-yasgui]').each(function(i) {
-      var $this = $(this)
-      const url = $this.attr('data-yasgui');
-      getFullUrl(url)
-        .then(linkUtils.getOptionsFromUrl)
-        .then(function(options) {
-          return cleanConfig(options,url)
-        })
-        .then(function(config) {
-          initializeWrapper($this)
-          window.$el = $this;
-          var yasgui = YASGUI($this, $.extend(config, {
-            //use persistencyPrefix so there are no conflicts between
-            //different yasgui instances
-              persistencyPrefix: function() {
-                return 'yasgui_stories_' + url
-              }
-            })
-          )
-          $this.extend({yasgui:yasgui})
-          if (!yasgui.current().yasr.results) {
-            yasgui.current().query()
-          }
-          window.yasgui = $this.yasgui;
-        })
-        .catch(console.error)
-    })
+    var yasguiDivs = $('div[data-yasgui]');
+    var count = 0;
+    function loadDivs() {
+
+      if (count >= yasguiDivs.length) {
+        //loaded everything, so we're done
+      } else {
+        loadDiv(yasguiDivs[count]).then(function() {
+          count++;
+          loadDivs();
+        });
+      }
+
+    }
+    loadDivs();
 });
 
 }
-
+function loadDiv(el) {
+  console.log('loading el',el)
+  var $this = $(el)
+  const url = $this.attr('data-yasgui');
+  return getFullUrl(url)
+    .then(linkUtils.getOptionsFromUrl)
+    .then(function(options) {
+      return cleanConfig(options,url)
+    })
+    .then(function(config) {
+      initializeWrapper($this)
+      window.$el = $this;
+      var yasgui = YASGUI($this, $.extend(config, {
+        //use persistencyPrefix so there are no conflicts between
+        //different yasgui instances
+          persistencyPrefix: function() {
+            return 'yasgui_stories_' + url
+          }
+        })
+      )
+      $this.extend({yasgui:yasgui})
+      if (!yasgui.current().yasr.results) {
+        yasgui.current().query()
+      }
+      window.yasgui = $this.yasgui;
+    })
+    .catch(console.error)
+}
 function cleanConfig(config, originalUrl) {
   if (config.yasqe.sparql && config.yasqe.sparql.endpoint && config.yasqe.sparql.endpoint.indexOf('http') !== 0) {
     //hmm, a relative path, do some magic to rewrite the endpoint
